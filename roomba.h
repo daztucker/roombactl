518d160e78a9dd32d2d7352857d90e3c95a44252
Darren Tucker <dtucker@zip.com.au>
2012-01-14 19:56:06 +1100

    Move magic numbers in a header

---
+++ roomba.h
@@ -0,0 +1,23 @@
+/*
+ * roomba commands, data from
+ * http://www.irobot.lv/uploaded_files/File/iRobot_Roomba_500_Open_Interface_Spec.pdf
+ */
+
+/* Initialisation commands */
+#define ROOMBA_START	128
+#define ROOMBA_BAUD	129
+
+/* Mode commands */
+#define ROOMBA_SAFE	131
+#define ROOMBA_FULL	132
+#define ROOMBA_POWER	133
+
+/* Cleaning commands */
+#define ROOMBA_SPOT	134
+#define ROOMBA_CLEAN	135
+#define ROOMBA_MAX	136
+#define ROOMBA_DOCK	143
+
+/* Schedule commands */
+#define ROOMBA_SCHEDULE	167
+#define ROOMBA_SETTIME	168
